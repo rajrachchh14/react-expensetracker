7e6f7fdc660f91c3b5148f9d2959b9129e566de2
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-01 13:29:16 +0530

    1 file modified

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function AppReducer() {
-  return <>test</>;
-}
+export default (state, action) => {
+  switch (action.type) {
+    // case 'increment':
+    //   return {count: state.count + 1};
+    // case 'decrement':
+    //   return {count: state.count - 1};
+    default:
+      return state;
+    // throw new Error();
+  }
+};
