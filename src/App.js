3148b4b91abc4bb6dfa6f9b1b4370488340ba892
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-01 11:18:32 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import IncomeExpense from './component/IncomeExpense';
 import TransactionList from './component/TransactionList';
 import TransactionForm from './component/TransactionForm';
+import Balence from './component/Balence';
 
 export default function App() {
   return (
     <>
       <div className="container mt-4">
         <h4 className="text-center mb-4">Expense Tracker </h4>
+        <Balence />
         <div className="row">
           <IncomeExpense />
           <TransactionList />
