2e970c2f9e84f00b1b58107778241bca50c58f66
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-01 11:23:01 +0530

    1 file modified

--- src/component/TransactionForm.js
+++ src/component/TransactionForm.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TransactionForm() {
+  const [title, setTitle] = useState('');
+  const [amount, setAsmount] = useState('');
   return (
     <>
       <div class="col-12 col-md-12 col-lg-4 pb-2">
         <form>
           <div class="mb-3">
-            <input type="text" class="form-control" placeholder="Title" />
+            <input
+              type="text"
+              class="form-control"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+            />
           </div>
           <div class="mb-3">
-            <input type="number" class="form-control" placeholder="Amount" />
+            <input
+              type="number"
+              class="form-control"
+              value={amount}
+              onChange={(e) => setAsmount(e.target.value)}
+              placeholder="Amount"
+            />
           </div>
 
           <button type="submit" class="btn btn-primary">
