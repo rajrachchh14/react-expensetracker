fb9afb47356371339950b71ba404c3f0b8aca64a
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-01 09:18:45 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
             <h3 className="text-center mb-4">My Bal $200.00 </h3>
           </div>
           <div class="col-4">
-            <h3 className="text-center">My 3 </h3>
+            <h3 className="text-center">Histtory </h3>
           </div>
           <div class="col-4">
             <h3 className="text-center">My 4 </h3>
@@ -31,7 +31,11 @@ export default function App() {
               </div>
             </div>
           </div>
-          <div class="col-4 col-lg-2">Column</div>
+          <div className="col-4">
+            <div className="alert alert-dark bd-callout bd-callout-danger">
+              A simple light alert—check it out!
+            </div>
+          </div>
           <div class="col-4 col-lg-2">Column</div>
           <div class="col-4 col-lg-2">Column</div>
           <div class="col-4 col-lg-2">Column</div>
