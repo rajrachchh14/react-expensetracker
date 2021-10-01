3392ed4d566253b12ffc68cb362007a5e97641c5
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-01 16:17:14 +0530

    1 file modified

--- src/component/Balence.js
+++ src/component/Balence.js
@@ -1,6 +1,7 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 export default function Balence() {
+  const { transaction } = useContext(GlobalContext);
   return (
     <>
       <h4 className="text-center">My Bal $200.00</h4>
