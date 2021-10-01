4116343d6432e6228f51478cb9137acdf4589ba0
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-01 16:07:04 +0530

    2 files modified

--- src/component/Balence.js
+++ src/component/Balence.js
@@ -4,14 +4,6 @@ export default function Balence() {
   return (
     <>
       <h4 className="text-center">My Bal $200.00</h4>
-      {/* 
-          
-          <div class="col-4">
-            <h4 className="text-center">History </h4>
-          </div>
-          <div class="col-4">
-            <h4 className="text-center">Add </h4>
-          </div> */}
     </>
   );
 }
