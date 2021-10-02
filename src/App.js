13bb4117a2200904b353841b1862524d485dbc10
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-02 12:33:40 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,28 +3,27 @@ import React from 'react';
 import IncomeExpense from './component/IncomeExpense';
 import TransactionList from './component/TransactionList';
 import TransactionForm from './component/TransactionForm';
-// import Balence from './component/Balence';
+import Balence from './component/Balence';
 import GlobalState from './context/GlobalState';
 
 export default function App() {
   return (
     <>
       {/* 3rd   This is Call Provider In Wrap Here Is Store Or Passing Data Globally Check If You Want Ro In component*/}
+      <GlobalState>
+        <div className="container mt-4">
+          <h4 className="text-center mb-2">Expense Tracker </h4>
+          <p className="text-center">Context Api with Create this app ... </p>
 
-      <div className="container mt-4">
-        <h4 className="text-center mb-2">Expense Tracker </h4>
-        <p className="text-center">Context Api with Create this app ... </p>
-        {/* <Balence /> */}
-
-        <div className="row">
-          <GlobalState>
+          <div className="row">
+            <Balence />
             <IncomeExpense />
 
             <TransactionList />
             <TransactionForm />
-          </GlobalState>
+          </div>
         </div>
-      </div>
+      </GlobalState>
     </>
   );
 }
--- src/component/Balence.js
+++ src/component/Balence.js
@@ -1,14 +1,15 @@
 // 5th
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
-export const Balence = () => {
+
+export default function Balence() {
   const { transaction } = useContext(GlobalContext); // List all Data
   const amt = transaction.map((transaction) => transaction.amount); // list all amount
   const total = amt.reduce((a, b) => (a += b), 0).toFixed(2); // Plus minus & Get Final Amount
-  // console.log(transaction);
+  console.log(amt);
   return (
     <>
-      <h4 className="text-center"> Balence {total} </h4>
+      <h4 className="text-center">Balence : {total}</h4>
     </>
   );
-};
+}
